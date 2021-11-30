307cefe3939d76bb150cf113e56011c3edf1393d
Agnese Spinella <agnesespinella@gmail.com>
2021-11-30 19:55:07 +0100

    fix: button back position fixed

--- js/index.js
+++ js/index.js
@@ -10,15 +10,7 @@ const loadCountries= async () => {
     const countries = await res.json();
     
      displayCountries(countries) 
-    
-     const btn = document.querySelector(".button")
-     btn.addEventListener("click", () => {
-         console.log("ciao")
-     })
-
     }
-
 
 /* Chiamata funzioni */
 loadCountries();
-
